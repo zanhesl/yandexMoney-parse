56b229aef4bc6c9bbdda7830fd3367c1604efd99
Alexander Lavrukhine <zanhesl@gmail.com>
2019-09-22 17:01:41 +0300

    uploaded project on github

---
+++ test.py
@@ -0,0 +1,83 @@
+import mechanize
+import http.cookiejar
+import requests
+from bs4 import BeautifulSoup
+import csv
+from datetime import datetime
+from multiprocessing import Pool
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+def get_html(br):
+	r = br.response()
+	data = r.read()
+	return data
+
+def get_page_data(html):
+	soup = BeautifulSoup(html, 'lxml')
+	balance = 'empty'
+	try:
+		balance = soup.find('span', class_='price__whole-amount text text_size_xxxxl text_view_primary text_weight_regular').text.strip()
+	except Exception as e:
+		print('No connection')
+	
+	return balance
+	
+def open_acc(login, password, number):
+	# Browser
+	br = mechanize.Browser()
+
+	# Cookie Jar
+	cj = http.cookiejar.LWPCookieJar()
+	br.set_cookiejar(cj)
+
+	# Browser options
+	br.set_handle_equiv(True)
+	br.set_handle_redirect(True)
+	br.set_handle_referer(True)
+	br.set_handle_robots(False)
+
+	# Follows refresh 0 but not hangs on refresh > 0
+	br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+
+	#br.set_debug_http(True)
+	#br.set_debug_redirects(True)
+	#br.set_debug_responses(True)
+
+	# User-Agent (this is cheating, ok?)
+	br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+
+	br.open("https://passport.yandex.ru/auth?origin=money&retpath=https%3A%2F%2Fmoney.yandex.ru%2F%3Ffrom%3Dauth")
+	br.select_form(nr=0)
+
+	br['login'] = login
+	br.select_form(nr=0)
+	br['passwd'] = password
+	br.submit()
+	br.select_form(nr=0)
+	br['answer'] = number
+	br.submit()
+	br.open('https://money.yandex.ru/actions')
+	
+
+		
+	
+
+	return get_page_data(get_html(br))
+
+	
+
+
+
+
+
+def main():
+	print(open_acc('refundhelper', 'tupoShkolnikam1337', '79312509544'), ' rub')
+		
+	# open_acc(br, 'd0lgoffsan@yandex.ru', 'zor767in')
+
+
+if __name__ == '__main__':
+	main()
